05f220254fd749c647f44b7593c606bdbb8d9439
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-07-11 21:03:49 -0500

    Add custom color 'crimson' to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        crimson: "#dc143c",
+      }
+    },
   },
   plugins: [],
 }
