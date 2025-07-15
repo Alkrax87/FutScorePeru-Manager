9333a700b3273c7f8b0f725e97a6a38963366117
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-07-14 20:21:00 -0500

    Refactor StadiumsApiService to manage stadium data with BehaviorSubject and implement CRUD operations

--- src/app/services/stadiums-api-service.service.ts
+++ src/app/services/stadiums-api-service.service.ts
@@ -1,17 +1,45 @@
 import { Injectable } from '@angular/core';
 import { Environments } from '../environment/environments';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
+import { Stadium } from '../interfaces/stadium';
 
 @Injectable({
   providedIn: 'root',
 })
-export class StadiumsApiServiceService {
+export class StadiumsApiService {
   private BackendUrl = Environments.backendUrl;
 
   constructor(private http: HttpClient) {}
 
-  getStadiums(): Observable<any> {
-    return this.http.get(this.BackendUrl + 'stadiums');
+  private stadiumsSubject = new BehaviorSubject<Stadium[]>([]);
+  dataStadiums$ = this.stadiumsSubject.asObservable();
+
+  getStadiums() {
+    this.http.get<Stadium[]>(this.BackendUrl + 'stadiums').subscribe({
+      next: (data) => (this.stadiumsSubject.next(data)),
+      error: (err) => (console.error('Failed to get Stadiums data ', err)),
+    });
+  }
+
+  addStadium(stadium: Stadium) {
+    this.http.post<Stadium>(this.BackendUrl + 'stadiums', stadium).subscribe({
+      next: () => (this.getStadiums()),
+      error: (err) => (console.error('Failed to add stadium: ', err.error.error)),
+    });
+  }
+
+  updateStadium(stadium: Stadium) {
+    this.http.put<Stadium>(this.BackendUrl + 'stadiums/' + stadium.stadiumId, stadium).subscribe({
+      next: () => (this.getStadiums()),
+      error: (err) => (console.error('Failed to update stadium: ', err.error.errors)),
+    });
+  }
+
+  deleteStadium(stadiumId: number) {
+    this.http.delete(this.BackendUrl + 'stadiums/' + stadiumId).subscribe({
+      next: () => (this.getStadiums()),
+      error: (err) => (console.error('Failed to delete stadium: ', err.error.error)),
+    });
   }
 }
