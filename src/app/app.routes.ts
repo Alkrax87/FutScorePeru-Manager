108a2e1e0d1435ef6c1aa058c79b22d691bc9171
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-07-19 16:38:50 -0500

    Refactor app routes to remove Liga components and streamline team routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { Liga1Component } from './pages/liga1/liga1.component';
-import { Liga2Component } from './pages/liga2/liga2.component';
-import { Liga3Component } from './pages/liga3/liga3.component';
 import { TeamPageComponent } from './pages/shared/team-page/team-page.component';
-import { TeamsListComponent } from './components/teams-list/teams-list.component';
 import { ManagersComponent } from './pages/managers/managers.component';
 import { StadiumsComponent } from './pages/stadiums/stadiums.component';
+import { TeamsComponent } from './pages/teams/teams.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -15,31 +12,12 @@ export const routes: Routes = [
     component: HomeComponent,
   },
   {
-    path: 'liga-1',
-    component: Liga1Component,
-    children: [
-      { path: '', redirectTo: 'teams', pathMatch: 'full' },
-      { path: 'teams', component: TeamsListComponent },
-      { path: 'team/:category/:teamId', component: TeamPageComponent },
-    ],
+    path: 'team/:category/:teamId',
+    component: TeamPageComponent
   },
   {
-    path: 'liga-2',
-    component: Liga2Component,
-    children: [
-      { path: '', redirectTo: 'teams', pathMatch: 'full' },
-      { path: 'teams', component: TeamsListComponent },
-      { path: 'team/:category/:teamId', component: TeamPageComponent },
-    ],
-  },
-  {
-    path: 'liga-3',
-    component: Liga3Component,
-    children: [
-      { path: '', redirectTo: 'teams', pathMatch: 'full' },
-      { path: 'teams', component: TeamsListComponent },
-      { path: 'team/:category/:teamId', component: TeamPageComponent },
-    ],
+    path: 'teams',
+    component: TeamsComponent,
   },
   {
     path: 'managers',
