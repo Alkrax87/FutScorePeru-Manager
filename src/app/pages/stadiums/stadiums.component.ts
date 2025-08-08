9f7b4abe0994b0b1a033de4e90576a6e1408d92b
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-08-07 20:50:29 -0500

    Refactor stadiums and teams components for improved layout and styling

--- src/app/pages/stadiums/stadiums.component.ts
+++ src/app/pages/stadiums/stadiums.component.ts
@@ -12,47 +12,51 @@ import { StadiumDeleteModalComponent } from "../../components/stadium-delete-mod
   selector: 'app-stadiums',
   imports: [FontAwesomeModule, StadiumAddModalComponent, StadiumEditModalComponent, StadiumDeleteModalComponent],
   template: `
-    <div class="px-20 my-10 flex justify-between">
-      <div>
-        <h2 class="text-3xl font-semibold">Stadium Management</h2>
-        <p class="text-neutral-500">Manage and view all stadiums</p>
-      </div>
-      <div class="flex items-center">
-        <button (click)="onAdd()" class="bg-night hover:bg-neutral-800 text-white px-4 py-2 rounded-xl">
-          <fa-icon [icon]="Add"></fa-icon> Add Stadium
-        </button>
-      </div>
-    </div>
-    <div class="px-20 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
-      @for (stadium of stadiums; track $index) {
-        <div class="border border-neutral-200 rounded-xl overflow-hidden shadow-md">
-          <div class="relative shadow-md">
-            <div class="bg-white text-neutral-700 rounded-lg text-sm shadow-md font-semibold px-3 absolute left-1.5 top-2">
-              StadiumId: {{ stadium.stadiumId }}
-            </div>
-            <img [src]="stadium.image" alt="STADIUM-image" class="w-full h-40 object-cover">
-          </div>
-          <div class="px-4 py-3">
-            <p class="font-semibold mb-2">{{ stadium.name }}</p>
-            <div class="text-neutral-500 text-sm flex gap-2">
-              <div class="w-4 text-center"><fa-icon [icon]="People"></fa-icon></div>
-              <p>{{ stadium.capacity }} capacity</p>
-            </div>
-            <div class="text-neutral-500 text-sm flex gap-2">
-              <div class="w-4 text-center"><fa-icon [icon]="Location"></fa-icon></div>
-              <p>{{ stadium.location }}</p>
-            </div>
-            <div class="flex gap-2 mt-2">
-              <button (click)="onEdit(stadium)" class="hover:bg-neutral-50 border border-neutral-200 w-full rounded-lg py-2 font-semibold text-sm">
-                <fa-icon [icon]="Edit"></fa-icon> Edit
-              </button>
-              <button (click)="onDelete(stadium)" class="bg-red-500 hover:bg-red-600 text-white border border-neutral-200 w-full rounded-lg py-2 font-semibold text-sm">
-                <fa-icon [icon]="Delete"></fa-icon> Delete
-              </button>
-            </div>
-          </div>
+    <div class="px-5 xl:px-32 py-5 sm:py-10 select-none">
+      <!-- Title -->
+      <div class="pb-4 flex flex-col sm:flex-row justify-between gap-4">
+        <div class="text-center sm:text-start">
+          <h2 class="text-3xl font-semibold">Stadium Management</h2>
+          <p class="text-neutral-500">Manage and view all stadiums</p>
         </div>
-      }
+        <div class="flex items-center">
+          <button (click)="onAdd()" class="bg-night hover:bg-neutral-800 text-white w-full sm:w-fit px-4 py-2 rounded-xl">
+            <fa-icon [icon]="Add"></fa-icon> Add Stadium
+          </button>
+        </div>
+      </div>
+      <!-- Content -->
+      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        @for (stadium of stadiums; track $index) {
+          <div class="border border-neutral-200 rounded-xl overflow-hidden shadow-md">
+            <div class="relative shadow-md">
+              <div class="bg-white text-neutral-700 rounded-full text-xs shadow-md font-semibold px-3 absolute left-1.5 top-2">
+                StadiumId: {{ stadium.stadiumId }}
+              </div>
+              <img [src]="stadium.image" alt="STADIUM-image" class="w-full h-40 object-cover">
+            </div>
+            <div class="p-3">
+              <p class="font-semibold mb-1">{{ stadium.name }}</p>
+              <div class="text-neutral-500 text-sm flex gap-2">
+                <div class="w-4 text-center"><fa-icon [icon]="People"></fa-icon></div>
+                <p>{{ stadium.capacity }} capacity</p>
+              </div>
+              <div class="text-neutral-500 text-sm flex gap-2">
+                <div class="w-4 text-center"><fa-icon [icon]="Location"></fa-icon></div>
+                <p>{{ stadium.location }}</p>
+              </div>
+              <div class="flex gap-2 mt-2">
+                <button (click)="onEdit(stadium)" class="hover:bg-neutral-50 border border-neutral-200 w-full rounded-lg py-2 font-semibold text-sm">
+                  <fa-icon [icon]="Edit"></fa-icon> Edit
+                </button>
+                <button (click)="onDelete(stadium)" class="bg-red-500 hover:bg-red-600 text-white border border-neutral-200 w-full rounded-lg py-2 font-semibold text-sm">
+                  <fa-icon [icon]="Delete"></fa-icon> Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        }
+      </div>
     </div>
 
     @if (showAddStadiumModal) {
--- src/app/pages/teams/teams.component.ts
+++ src/app/pages/teams/teams.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faArrowUpRightFromSquare, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faArrowUpRightFromSquare, faLocationDot, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { Subscription } from 'rxjs';
 import { TeamsApiService } from '../../services/teams-api.service';
 import { StadiumsApiService } from '../../services/stadiums-api-service.service';
@@ -12,35 +12,37 @@ import { TeamAddModalComponent } from '../../components/team-add-modal/team-add-
   selector: 'app-teams',
   imports: [FontAwesomeModule, RouterLink, TeamAddModalComponent],
   template: `
-    <div class="px-20 py-10 select-none">
-      <div class="pb-10 flex justify-between">
-        <div>
+    <div class="px-5 xl:px-32 py-5 sm:py-10 select-none">
+      <!-- Title -->
+      <div class="pb-4 flex flex-col sm:flex-row justify-between gap-4">
+        <div class="text-center sm:text-start">
           <h2 class="text-3xl font-semibold">Team Management</h2>
           <p class="text-neutral-500">Manage and view all teams</p>
         </div>
         <div class="flex items-center">
-          <button (click)="onAdd()" class="bg-night hover:bg-neutral-800 text-white px-4 py-2 rounded-xl">
+          <button (click)="onAdd()" class="bg-night hover:bg-neutral-800 text-white w-full sm:w-fit px-4 py-2 rounded-xl">
             <fa-icon [icon]="Add"></fa-icon> Add Team
           </button>
         </div>
       </div>
-      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+      <!-- Content -->
+      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         @for (team of teams; track $index) {
-          <div [routerLink]="['/team',team.category, team.teamId]" class="border border-neutral-200 hover:bg-neutral-50 duration-300 group rounded-xl overflow-hidden shadow-md cursor-pointer">
+          <div [routerLink]="['/team',team.category, team.teamId]" class="border border-neutral-200 hover:bg-neutral-50 duration-300 group rounded-lg overflow-hidden shadow-md cursor-pointer">
             <div [style.backgroundColor]="team.color.c1" class="h-3"></div>
-            <div class="p-4">
+            <div class="px-4 py-4">
               <div class="flex">
                 <div class="w-full">
                   <p class="text-neutral-400 text-xs">TeamID: {{ team.teamId }}</p>
                   <div class="flex items-center gap-2 mt-2">
                     <img [src]="team.image" [alt]="team.alt" class="w-12 h-12">
-                    <div>
-                      <p class="font-semibold">{{ team.name }}</p>
-                      <p class="text-neutral-500 text-xs">{{ team.location }}</p>
+                    <div class="truncate">
+                      <p class="font-semibold text-sm truncate min-w-32">{{ team.name }}</p>
+                      <p class="text-neutral-500 text-xs"><fa-icon [icon]="Location"></fa-icon> {{ team.location }}</p>
                     </div>
                   </div>
                 </div>
-                <div class="flex items-center text-neutral-500">
+                <div class="flex items-center text-neutral-500 text-sm">
                   <fa-icon class="py-2 px-3 group-hover:bg-crimson group-hover:text-white rounded-full duration-300" [icon]="Redirect"></fa-icon>
                 </div>
               </div>
@@ -67,6 +69,7 @@ export class TeamsComponent {
 
   Add = faPlus;
   Redirect = faArrowUpRightFromSquare;
+  Location = faLocationDot;
 
   showAddTeamModal = false;
 
