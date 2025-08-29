e8db46a8f65f854a69c1bc0e7ea0be4b593bffd2
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-08-28 20:05:34 -0500

    Add footer component to main app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { MainNavComponent } from './components/main-nav/main-nav.component';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainNavComponent],
+  imports: [RouterOutlet, MainNavComponent, FooterComponent],
   template: `
     <app-main-nav></app-main-nav>
-    <div class="pt-16">
-      <router-outlet></router-outlet>
-    </div>
+    <router-outlet></router-outlet>
+    <app-footer></app-footer>
   `,
   styles: ``,
 })
