1cbc9a18dd829da0da8ae75448c4efc01581bc7f
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-08-28 20:03:44 -0500

    Add footer component with author information

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  template: `
+    <div class="py-2 text-white text-center bg-crimson font-semibold">
+      <a href="https://www.mavp_projects.com">&copy; 2025 MAVP Projects</a>
+    </div>
+  `,
+  styles: ``,
+})
+export class FooterComponent {}
