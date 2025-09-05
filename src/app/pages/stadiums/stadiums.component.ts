68009b5c369d3c13957387e13637cfc62d5c0059
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-09-04 19:54:53 -0500

    Update StadiumsComponent styles and icons for improved UI/UX

--- src/app/pages/stadiums/stadiums.component.ts
+++ src/app/pages/stadiums/stadiums.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, signal } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faLocationDot, faPenToSquare, faPlus, faTrashCan, faUsers } from '@fortawesome/free-solid-svg-icons';
+import { faLocationDot, faPenToSquare, faPlus, faTrashCan, faUserGroup, faUsers } from '@fortawesome/free-solid-svg-icons';
 import { StadiumsApiService } from '../../services/stadiums-api.service';
 import { Stadium } from '../../interfaces/stadium';
 import { Subscription } from 'rxjs';
@@ -19,7 +19,7 @@ import { DeleteConfirmationModalComponent } from "../../components/delete-confir
           <p class="text-neutral-500">Manage and view all stadiums</p>
         </div>
         <div class="flex items-center">
-          <button (click)="onAdd()" class="bg-night hover:bg-neutral-800 text-white w-full sm:w-fit px-4 py-2 rounded-xl">
+          <button (click)="onAdd()" class="bg-green-700 hover:bg-green-700/90 text-white w-full sm:w-fit px-6 py-2 rounded-full">
             <fa-icon [icon]="Add"></fa-icon> Add Stadium
           </button>
         </div>
@@ -27,29 +27,27 @@ import { DeleteConfirmationModalComponent } from "../../components/delete-confir
       <!-- Content -->
       <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         @for (stadium of stadiums; track $index) {
-          <div class="border border-neutral-200 rounded-xl overflow-hidden shadow-md">
-            <div class="relative shadow-md">
-              <div class="bg-white text-neutral-700 rounded-full text-xs shadow-md font-semibold px-3 absolute left-1.5 top-2">
-                StadiumId: {{ stadium.stadiumId }}
+          <div class="group rounded-3xl overflow-hidden shadow-md">
+            <div class="relative shadow-md overflow-hidden">
+              <div class="bg-white bg-opacity-90 text-neutral-700 flex gap-1.5 z-10 rounded-full text-xs shadow-md font-semibold px-2 py-0.5 absolute right-3 top-3">
+                <fa-icon style="font-size: 10px;" [icon]="People"></fa-icon>
+                <p>{{ formatNumber(stadium.capacity!) }}</p>
               </div>
-              <img [src]="stadium.image" alt="STADIUM-image" class="w-full h-40 object-cover">
+              <img [src]="stadium.image" alt="STADIUM-image" class="group-hover:scale-105 duration-300 w-full h-48 object-cover">
             </div>
-            <div class="p-3">
-              <p class="font-semibold mb-1">{{ stadium.name }}</p>
-              <div class="text-neutral-500 text-sm flex gap-2">
-                <div class="w-4 text-center"><fa-icon [icon]="People"></fa-icon></div>
-                <p>{{ stadium.capacity }} capacity</p>
-              </div>
-              <div class="text-neutral-500 text-sm flex gap-2">
-                <div class="w-4 text-center"><fa-icon [icon]="Location"></fa-icon></div>
+            <div class="p-4">
+              <p class="text-xs">ID: {{ stadium.stadiumId }}</p>
+              <p class="font-semibold">{{ stadium.name  }} </p>
+              <div class="text-neutral-500 text-sm flex gap-1">
+                <fa-icon class="text-xs" [icon]="Location"></fa-icon>
                 <p>{{ stadium.location }}</p>
               </div>
-              <div class="flex gap-2 mt-2">
-                <button (click)="onEdit(stadium)" class="hover:bg-neutral-50 border border-neutral-200 w-full rounded-lg py-2 font-semibold text-sm">
+              <div class="flex justify-end gap-2 mt-2">
+                <button (click)="onEdit(stadium)" class="hover:bg-neutral-100/50 text-neutral-600 border w-full rounded-full py-2 text-sm duration-300">
                   <fa-icon [icon]="Edit"></fa-icon> Edit
                 </button>
-                <button (click)="onDelete(stadium)" class="bg-red-500 hover:bg-red-600 text-white border border-neutral-200 w-full rounded-lg py-2 font-semibold text-sm">
-                  <fa-icon [icon]="Delete"></fa-icon> Delete
+                <button (click)="onDelete(stadium)" class="bg-red-600 text-white hover:bg-red-600/80 rounded-full px-4 py-2 text-sm duration-300">
+                  <fa-icon [icon]="Delete"></fa-icon>
                 </button>
               </div>
             </div>
@@ -88,7 +86,8 @@ export class StadiumsComponent {
 
   selectedStadium = signal<Stadium | null>(null);
 
-  People = faUsers;
+  // People = faUsers;
+  People = faUserGroup;
   Location = faLocationDot;
   Add = faPlus;
   Edit = faPenToSquare;
@@ -99,6 +98,10 @@ export class StadiumsComponent {
     this.StadiumSubscription = this.stadiumsService.dataStadiums$.subscribe({
       next: (data) => (this.stadiums = data),
     });
+  }
+
+  formatNumber(num: number) {
+    return new Intl.NumberFormat("en-US").format(num);
   }
 
   onAdd() {
