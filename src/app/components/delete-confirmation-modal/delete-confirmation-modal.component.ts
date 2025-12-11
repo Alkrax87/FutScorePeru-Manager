a054d6ae0b2fd9a6a5c11994cabb6a4231e6d8c1
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-12-11 18:52:39 -0500

    Improve DeleteConfirmationModal styling and layout

--- src/app/components/delete-confirmation-modal/delete-confirmation-modal.component.ts
+++ src/app/components/delete-confirmation-modal/delete-confirmation-modal.component.ts
@@ -6,13 +6,13 @@ import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
   selector: 'app-delete-confirmation-modal',
   imports: [FontAwesomeModule],
   template: `
-    <div class="bg-black bg-opacity-70 fixed inset-0 z-50 flex justify-center items-center select-none px-5">
-      <div class="bg-white p-4 rounded-3xl w-full max-w-sm">
-        <h3 class="text-xl font-semibold">Delete {{ message.section }}</h3>
-        <p class="text-neutral-800 text-sm text-justify">
+    <div class="bg-black bg-opacity-70 fixed inset-0 z-50 flex justify-center items-center select-none px-3 sm:px-5">
+      <div class="bg-white p-5 rounded-3xl w-full max-w-sm">
+        <h3 class="text-2xl font-semibold">Delete {{ message.section }}</h3>
+        <p class="text-neutral-800 text-sm text-justify mt-2 mb-4">
           Are you sure you want to delete <span class="font-semibold">{{ message.element }}?</span> This action cannot be undone.
         </p>
-        <div class="flex justify-end gap-2 mt-3">
+        <div class="flex justify-end gap-2">
           <button type="button" (click)="close.emit()" class="hover:bg-neutral-100/50 text-neutral-600 border rounded-full px-4 py-2 text-sm duration-300">Cancelar</button>
           <button type="submit" (click)="confirm.emit()" class="bg-red-600 text-white hover:bg-red-600/80 rounded-full px-4 py-2 text-sm duration-300">
             <fa-icon [icon]="Delete"></fa-icon>&nbsp; Delete
@@ -24,7 +24,7 @@ import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
   styles: ``,
 })
 export class DeleteConfirmationModalComponent {
-  @Input() message!: { section: string, element: string };
+  @Input() message!: { section: string; element: string };
   @Output() confirm = new EventEmitter<void>();
   @Output() close = new EventEmitter<void>();
 
