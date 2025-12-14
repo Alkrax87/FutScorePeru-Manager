10e5ea453c703d55cee7612654049805dd828a71
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-12-13 20:13:45 -0500

    Refactor for improved UI and update status handling

--- src/app/components/map-update-modal/map-update-modal.component.ts
+++ src/app/components/map-update-modal/map-update-modal.component.ts
@@ -1,36 +1,40 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { MapElement } from '../../interfaces/map-element';
-import { faPenToSquare, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-map-update-modal',
   imports: [FontAwesomeModule],
   template: `
-    <div class="bg-black bg-opacity-70 fixed inset-0 z-50 flex justify-center items-center select-none">
-      <div class="bg-white p-5 rounded-xl w-full max-w-sm">
-        <h3 class="text-lg font-semibold">Change Map Element status</h3>
-        <p class="text-neutral-500 text-sm text-justify">Are you sure you want to change <span class="font-semibold">{{ map }}</span> status?</p>
-        <div class="flex gap-2 justify-end mt-4">
-          <button (click)="onCancel()" class="hover:bg-neutral-50 border border-neutral-200 rounded-lg px-5 py-1.5 font-semibold text-sm">
-            <fa-icon [icon]="Cancel"></fa-icon> Cancel
-          </button>
-          <button (click)="onUpdate()" class="bg-night hover:bg-neutral-800 text-white border-neutral-200 rounded-lg px-5 py-1.5 font-semibold text-sm">
-            <fa-icon [icon]="Edit"></fa-icon> Update Status
-          </button>
+    <div class="bg-black bg-opacity-70 fixed inset-0 z-50 flex justify-center items-center select-none px-3 sm:px-5">
+      <div class="bg-white p-5 rounded-3xl w-full max-w-sm">
+        <h3 class="text-xl  font-semibold">Change Region status</h3>
+        <p class="text-neutral-800 text-sm text-justify mt-2 mb-4">
+          Are you sure you want to change <span class="font-semibold">{{ mapItem.region }}</span> status?
+        </p>
+        <div class="flex justify-end gap-2">
+          <button (click)="onCancel()" class="hover:bg-neutral-100/50 text-neutral-600 border rounded-full px-4 py-2 text-sm duration-300">Cancel</button>
+          @if (mapItem.status) {
+            <button (click)="onUpdate()" class="bg-red-600 hover:hover:bg-opacity-85 text-white rounded-full px-4 py-2 text-sm duration-300">
+              <fa-icon [icon]="Edit"></fa-icon>&nbsp; Disable
+            </button>
+          } @else {
+            <button (click)="onUpdate()" class="bg-green-600 hover:hover:bg-opacity-85 text-white rounded-full px-4 py-2 text-sm duration-300">
+              <fa-icon [icon]="Edit"></fa-icon>&nbsp; Enable
+            </button>
+          }
         </div>
       </div>
     </div>
   `,
-  styles: ``
+  styles: ``,
 })
 export class MapUpdateModalComponent {
-  @Input() map!: string;
+  @Input() mapItem!: { region: string, status: boolean };
   @Output() update = new EventEmitter<void>();
   @Output() cancel = new EventEmitter<void>();
 
   Edit = faPenToSquare;
-  Cancel = faXmark;
 
   onUpdate() {
     this.update.emit();
