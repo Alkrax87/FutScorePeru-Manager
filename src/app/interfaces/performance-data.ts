6f18b9738a50ecc1c16506deba15f0bdc3e176ce
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2026-01-02 13:34:52 -0500

    Refactor interface to replace optional attributes and standarize content with phase1 and phase2

--- src/app/interfaces/performance-data.ts
+++
@@ -1,9 +0,0 @@
-export interface PerformanceData {
-  w: number;
-  d: number;
-  l: number;
-  gf: number;
-  ga: number;
-  addition?: number;
-  sanction: number;
-}
--- src/app/interfaces/team-performance.ts
+++ src/app/interfaces/team-performance.ts
@@ -1,61 +1,20 @@
 export interface TeamPerformance {
   teamId: string;
-  category: number;
-  apertura?: {
-    points: number;
-    played: number;
+  phase1: {
     w: number;
     d: number;
     l: number;
     gf: number;
     ga: number;
-    gd: number;
     sanction: number;
-  };
-  clausura?: {
-    points: number;
-    played: number;
+  },
+  phase2: {
     w: number;
     d: number;
     l: number;
     gf: number;
     ga: number;
-    gd: number;
     sanction: number;
-  };
-  regional?: {
-    points: number;
-    played: number;
-    w: number;
-    d: number;
-    l: number;
-    gf: number;
-    ga: number;
-    gd: number;
-    sanction: number;
-  };
-  grupos?: {
-    points: number;
-    played: number;
-    w: number;
-    d: number;
-    l: number;
-    gf: number;
-    ga: number;
-    gd: number;
     addition: number;
-    sanction: number;
-  };
-  final?: {
-    points: number;
-    played: number;
-    w: number;
-    d: number;
-    l: number;
-    gf: number;
-    ga: number;
-    gd: number;
-    addition: number;
-    sanction: number;
-  };
+  }
 }
